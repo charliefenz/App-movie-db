52e2bf545041c979c69681b5789267e20d5c34fa
charliefenz <fentecarlos@gmail.com>
2021-05-13 07:45:45 +0200

    footer created

--- app-movie-db/src/app/app.module.ts
+++ app-movie-db/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { MainComponent } from './components/layout/main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/app/modules/material/material.module';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
