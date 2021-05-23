e88f7735e00080e54acae29117e7a0a0fc7b5521
charliefenz <fentecarlos@gmail.com>
2021-05-23 13:53:25 +0200

    added TODO Tree extension and placed TODOs for latter improvements

--- app-movie-db/src/app/modules/movies/components/list-popular-movies/list-popular-movies.component.ts
+++ app-movie-db/src/app/modules/movies/components/list-popular-movies/list-popular-movies.component.ts
@@ -9,6 +9,9 @@ import { MoviesService } from '../../services/movies.service';
 })
 export class ListPopularMoviesComponent implements OnInit {
 
+  // TODO Limit initial load to viewport width
+/* Maybe with a formula that detects vw, divides by fixed px of element and indicates the array how many items should show, then with a clickable "view more" should expand the list */
+
   popularMovies: MovieListResult[];
 
   constructor(private moviesService: MoviesService) { }
