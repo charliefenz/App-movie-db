0a0568d9b53191c8aab6fa38124a5f51e8950227
charliefenz <fentecarlos@gmail.com>
2021-06-24 15:18:16 +0200

    search result component created with routing configured

--- app-movie-db/src/app/app-routing.module.ts
+++ app-movie-db/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./modules/movies-and-series/movies-and-series.module').then((m) => m.MoviesAndSeriesModule)},
   {path: 'movies', loadChildren: () => import('./modules/movies/movies.module').then((m) => m.MoviesModule)},
-  {path: 'series', loadChildren: () => import('./modules/series/series.module').then((m) => m.SeriesModule)}
+  {path: 'series', loadChildren: () => import('./modules/series/series.module').then((m) => m.SeriesModule)},
+  {path: 'search', component: SearchResultComponent}
 ];
 
 @NgModule({
