31c3f503c279bc2d1fd0d06c1948a8c13e16bc79
charliefenz <fentecarlos@gmail.com>
2022-03-02 18:03:46 +0100

    Feat: Add properties intentioned to provide change detections

--- app-movie-db/src/app/modules/search/services/search.service.ts
+++ app-movie-db/src/app/modules/search/services/search.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable} from 'rxjs';
 import { EndPoints } from 'src/app/common/classes/endpoints';
 import { SearchResponseObject } from '../models/search-response-object';
 
@@ -8,6 +8,11 @@ import { SearchResponseObject } from '../models/search-response-object';
   providedIn: 'root'
 })
 export class SearchService {
+  
+  private _detector = 0;
+  private _changeDetection = new BehaviorSubject<number>(this._detector);
+  
+  public changeDetection = this._changeDetection.asObservable();
 
   constructor(private httpClient: HttpClient) { }
 
