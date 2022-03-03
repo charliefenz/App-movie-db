330c6905ad7890b71ef72481e1a7ed4fe2f5e147
charliefenz <fentecarlos@gmail.com>
2022-03-03 16:24:36 +0100

    Fix: refactor getSearchResults() on search-result component

--- app-movie-db/src/app/modules/search/components/search-result/search-result.component.ts
+++ app-movie-db/src/app/modules/search/components/search-result/search-result.component.ts
@@ -41,100 +41,33 @@ export class SearchResultComponent implements OnInit {
     });
   }
 
-  checkValidResponse(): void {
-    const searchResponse$ = this.searchService.getSearchResults(this.valueToSearch, '1');
-    searchResponse$.subscribe((res) => {
-      if (res.total_results === 0) {
+  getSearchResults(searchPage: number): void {
+    const routeQuery$ = this.route.queryParams;
+    routeQuery$
+    .pipe(
+      mergeMap((queryObject) => {
+        this.valueToSearch = queryObject.query;
+        return this.searchService.getSearchResults(this.valueToSearch, searchPage.toString());
+      })
+    )
+    .subscribe((searchResponseObject) => {
+      if (searchResponseObject.total_pages === 0) {
         this.existingReturn = false;
       } else {
-        this.existingReturn = true;
+        this.fillProperties(searchResponseObject);
       }
+      this.searchResponseObjectTotalItems = searchResponseObject.total_results;
+      this.searchService.informCompleteReception();
     });
   }
 
-  //TODO: This needs to be clearer
-  
-  getSearchResults(): void {
-    this.searchReturn = [];
-    let pagesToSearch: number;
-
-    //TODO Test how to sort the response by popularity
-
-
-    const totalSeriesPages$ = this.seriesService.getSearchSeries(1, this.valueToSearch);
-    const totalMoviesPages$ = this.moviesService.getSearchMovies(1, this.valueToSearch);
-
-        // if (this.searchTotalPages <= restotalSeriesPages.total_pages) {
-        //   pagesToSearch = this.searchTotalPages;
-        // } else {
-        //   pagesToSearch = restotalSeriesPages.total_pages;
-        // }
-        // Loop of subscriptions to the API to firstly get the series response for each API page
-        for (let i = 1; i <= pagesToSearch; i++) {
-          const seriesSearch$ = this.seriesService.getSearchSeries(i, this.valueToSearch);
-          seriesSearch$.subscribe((resSeriesSearch) => {
-            const searchSeriesResults = this.createSearchReturnArray(resSeriesSearch.results, 'tv');
-            this.searchReturn = this.searchReturn.concat(searchSeriesResults); // Extracting the results to the local component array
-            // If statement to instruct to go for the mmovies response once it has finished with the series response
-            if (i === pagesToSearch) {
-              // First subscription to check the max length of the returned array
-              totalMoviesPages$.subscribe((restotalMoviesPages) => {
-                if (this.searchTotalPages <= restotalMoviesPages.total_pages) {
-                  pagesToSearch = this.searchTotalPages;
-                } else {
-                  pagesToSearch = restotalMoviesPages.total_pages;
-                }
-                // 2nd Loop of subscriptions to the API to get the movies response for each API page
-                for (let j = 1; j <= pagesToSearch; j++) {
-                  const MoviesSearch$ = this.moviesService.getSearchMovies(i, this.valueToSearch);
-                  MoviesSearch$.subscribe((resMoviesSearch) => {
-                    const searchMoviesResults = this.createSearchReturnArray(resMoviesSearch.results, 'movie');
-                    this.searchReturn = this.searchReturn.concat(searchMoviesResults); // Adding the results to the local component array
-                  });
-                }
-              });
-            }          
-          });
-        }
+  fillProperties(searchResponseObject: SearchResponseObject): void {
+    this.rawSearchResponseLength = searchResponseObject.results.length;
+    this.searchResponseCleaned = this.removePersonsFromSearchResult(searchResponseObject.results);
+    this.searchResponseCleanedLength = this.searchResponseCleaned.length;
   }
 
-  private createSearchReturnArray(resArray: any, inputType: string): SearchReturn[] {
-    let posterPath: string;
-    const response: SearchReturn[] = [];
-    // If statement to accurately ask for the title or the name depending on wether it is treating a series or a movie
-    if (inputType === 'tv') {
-      for (const resObject of resArray) {
-        if (resObject.poster_path !== null) {
-          posterPath = GlobalConstants.imagesPosterUrl + resObject.poster_path;
-        } else {
-          posterPath = '../../../assets/images/no-image.png';
-        }
-        const searchObject: SearchReturn = {
-          type: inputType,
-          id: resObject.id,
-          nameOrTitle: resObject.name,
-          popularity: resObject.popularity,
-          poster_path: posterPath
-        };
-        response.push(searchObject);
-      }
-    } else {
-      for (const resObject of resArray) {
-        if (resObject.poster_path !== null) {
-          posterPath = GlobalConstants.imagesPosterUrl + resObject.poster_path;
-        } else {
-          posterPath = '../../../assets/images/no-image.png';
-        }
-        const searchObject: SearchReturn = {
-          type: inputType,
-          id: resObject.id,
-          nameOrTitle: resObject.title,
-          popularity: resObject.popularity,
-          poster_path: posterPath
-        };
-        response.push(searchObject);
-      }
-    }
-    return response;
+  removePersonsFromSearchResult(rawSearchResponse: SearchResponse[]): SearchResponse[] {
+    return rawSearchResponse.filter(searchItem => searchItem.media_type !== this.mediaTypePerson);
   }
 }
