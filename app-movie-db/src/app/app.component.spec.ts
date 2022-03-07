b8be0c6c694a4d8b206a1fd72874e2cc9c6c2102
charliefenz <fentecarlos@gmail.com>
2022-03-07 16:44:55 +0100

    Add standard config for app-component-spec

--- app-movie-db/src/app/app.component.spec.ts
+++ app-movie-db/src/app/app.component.spec.ts
@@ -1,16 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SearchBarComponent } from './components/layout/search-bar/search-bar.component';
+import { FocusDirective } from './directives/focus.directive';
+import { MaterialModule } from './modules/material/material.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        SearchBarComponent,
+        FocusDirective
       ],
+      providers: []
     }).compileComponents();
   });
 
@@ -24,12 +44,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app-movie-db');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app-movie-db app is running!');
   });
 });
