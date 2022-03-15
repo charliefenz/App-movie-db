53ff72dafa068f9332a59bb31895502e01d8575e
charliefenz <fentecarlos@gmail.com>
2022-03-15 08:31:17 +0100

    Fix: Change name of search models

--- app-movie-db/src/app/modules/search/models/search-response.ts
+++ app-movie-db/src/app/modules/search/models/search-response.ts
@@ -1,27 +1,10 @@
-import { PeopleResponse } from "./array-search-people-response";
+import { SearchResults } from "./search-results";
 
 export interface SearchResponse {
-    adult?: boolean;
-    backdrop_path?: string;
-    genre_ids?: number[];
-    id?: number;
-    media_type: string;
-    original_language?: string;
-    original_title?: string;
-    overview?: string;
-    popularity: number;
-    poster_path?: string;
-    release_date?: string;
-    title?: string;
-    video?: boolean;
-    vote_average?: number;
-    vote_count?: number;
-    gender?: number;
-    known_for?: PeopleResponse[];
-    known_for_department?: string;
-    name?: string;
-    profile_path?: string;
-    first_air_date?: string;
-    origin_country?: string[];
-    original_name?: string;
+    page: number;
+    results: SearchResults[];
+    total_pages: number;
+    total_results: number;
 }
+
+
