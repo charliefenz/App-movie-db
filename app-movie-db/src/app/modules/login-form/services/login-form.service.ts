064f4ef14961cc41e0e73143a88b0ae8b5ccd066
charliefenz <fentecarlos@gmail.com>
2022-03-23 16:46:31 +0100

    Feat: Create getUsers and login methods using mock JSONs

--- app-movie-db/src/app/modules/login-form/services/login-form.service.ts
+++ app-movie-db/src/app/modules/login-form/services/login-form.service.ts
@@ -1,7 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { UserLoginInfo } from '../models/user-login-info';
+import { UsersResponse } from '../models/users-response';
 
 @Injectable()
 export class LoginFormService {
 
-  constructor() { }
+  mockUrl = 'assets/mocks/users.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Observable<UsersResponse> {
+    return this.httpClient.get<UsersResponse>(this.mockUrl);
+  }
+  
+  async login(userInfo: UserLoginInfo): Promise<boolean> {
+    let usersResponse = await this.getUsers().toPromise();
+    let userFound: User | undefined;
+    let response = false;
+
+    userFound = usersResponse.users.find(user => user.email === userInfo.email && user.password === userInfo.password)
+
+    if (userFound) {
+      return response = true;
+    } else {
+      return response;
+    }
+  }
 }
