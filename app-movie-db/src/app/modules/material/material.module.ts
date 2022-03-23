c242774a79393c894e825a40543720e16097acfb
charliefenz <fentecarlos@gmail.com>
2022-03-23 10:46:40 +0100

    Feat: Change html and css to incorporate material elements

--- app-movie-db/src/app/modules/material/material.module.ts
+++ app-movie-db/src/app/modules/material/material.module.ts
@@ -4,8 +4,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
